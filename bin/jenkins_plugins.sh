9eb54100a53840f722cf86f694b5fd9a90a7a0d5
Benjamin Hipple <bhipple@bloomberg.net>
2016-09-19 11:49:27 -0400

    alias less as less -N; jenkins plugin script

---
+++ bin/jenkins_plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to get all of the installed plugins and versions of those plugins on Jenkins,
+# from the cmdline.
+
+jenkins_plugins=/var/lib/jenkins/plugins
+for plugin in $("$jenkins_plugins/*.jpi"); do
+    plugin=$(basename "$plugin" .jpi)
+    manifest="$jenkins_plugins/$plugin/META-INF/MANIFEST.MF"
+    echo "$plugin" $(grep 'Plugin-Version' "$manifest" | awk '{print $2}')
+done
