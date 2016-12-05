2e4f5e04525afd4fa87cc85b504cb8dff4063a47
Benjamin Hipple <benjamin.hipple@gmail.com>
2016-12-04 22:41:19 -0500

    Refactor NixPkgs config.nix

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,35 +1,48 @@
 {
+  allowUnfree = true;
+
   packageOverrides = pkgs_: with pkgs_; {
-    all = with pkgs; hiPrio (buildEnv {
-      name = "all";
+    # Minimal set of packages to install everywhere
+    minEnv = with pkgs; hiPrio (buildEnv {
+      name = "minEnv";
       paths = [
-        clang
-        cmake
+        bash
+        coreutils
         curl
         emacs
         file
         git
-        gnumake
+        gnutar
         htop
+        lftp
         neovim
+        nix-repl
         par
         tmux
         tree
+        unzip
         wget
         zsh
       ];
     });
 
+    # For "permanent" systems
     bigEnv = with pkgs; hiPrio (buildEnv {
       name = "bigEnv";
       paths = [
+        minEnv
         calibre
+        cmake
+        dropbox
+        gnumake
         graphviz
         imagemagick
         irssi
         mupdf
+        nixops
         pandoc
         shellcheck
+        stack
         vagrant
         vlc
         xclip
