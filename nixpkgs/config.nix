3521b5116ee60a50b8c60c25a0354b1b1d506851
Benjamin Hipple <bhipple@bloomberg.net>
2016-12-07 11:40:15 -0500

    Working on fixing up mac env

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,7 +1,22 @@
 {
   allowUnfree = true;
 
-  packageOverrides = pkgs_: with pkgs_; {
+  packageOverrides = pkgs: with pkgs; {
+    # Temporary fix for MacOS Sierra. Remove this block once the issues
+    # with Jemalloc are resolved.
+    neovim = pkgs.lib.overrideDerivation (pkgs.neovim.override {
+      withJemalloc = false;
+    }) (attrs: rec {
+      name = "neovim-${version}";
+      version = "0.1.7";
+      src = pkgs.fetchFromGitHub {
+        owner = "neovim";
+        repo = "neovim";
+        rev = "0542baac28681050483c685c79efcb4d3c1e32ea";
+        sha256 = "0bk0raxlb1xsqyw9pmqmxvcq5szqhimidrasnvzrci84gld8cwz4";
+      };
+    });
+
     # Minimal set of packages to install everywhere
     minEnv = with pkgs; hiPrio (buildEnv {
       name = "minEnv";
@@ -14,8 +29,6 @@
         git
         gnutar
         htop
-        lftp
-        neovim
         nix-repl
         par
         tmux
@@ -26,28 +39,37 @@
       ];
     });
 
-    # For "permanent" systems
+    # For "permanent" systems; compatible on both Mac and Linux
     bigEnv = with pkgs; hiPrio (buildEnv {
       name = "bigEnv";
       paths = [
         minEnv
-        calibre
         cmake
-        dropbox
         gnumake
         graphviz
         imagemagick
         irssi
-        mupdf
         nixops
         pandoc
         shellcheck
         stack
         vagrant
-        vlc
         xclip
         xsel
-#texlive-combined-full-2016
+      ];
+    });
+
+    # For "permanent" systems; compatible on both Mac and Linux
+    bigEnvLinux = with pkgs; hiPrio (buildEnv {
+      name = "bigEnvLinux";
+      paths = [
+        bigEnv
+        calibre
+        dropbox
+        mupdf
+        vlc
+        lftp
+        texlive-combined-full-2016
       ];
     });
   };
