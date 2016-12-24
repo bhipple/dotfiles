c8ba76d60bd1614901899e6971e064aa218e3262
Benjamin Hipple <benjamin.hipple@gmail.com>
2016-12-24 00:26:42 -0500

    Working with zsh surround

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -145,3 +145,16 @@ zle-keymap-select () {
   my-reset-prompt-maybe
 }
 zle -N zle-keymap-select
+
+#  ============================================================================
+#                          Text Object Surround
+#  ============================================================================
+autoload -Uz surround
+
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -a cs change-surround
+bindkey -a ds delete-surround
+bindkey -a ys add-surround
+bindkey -M visual S add-surround
