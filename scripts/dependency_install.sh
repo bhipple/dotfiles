97fbe3d2bafbef4cbb6b85b09de699435d48385b
Benjamin Hipple <bhipple@bloomberg.net>
2017-02-06 15:37:35 -0500

    update dependency installer

--- scripts/dependency_install.sh
+++ scripts/dependency_install.sh
@@ -1,28 +1,33 @@
-#!/bin/bash
-##########################################
-# Install the Chef Developer Kit
-#
-if [ -z "$(which chef-apply)" ]; then
-    echo "Installing Chef DK"
-    wget https://opscode-omnibus-packages.s3.amazonaws.com/debian/6/x86_64/chefdk_0.9.0-1_amd64.deb
-    sudo dpkg -i chefdk_0.9.0-1_amd64.deb
-    rm chefdk_0.9.0-1_amd64.deb
-else
-    echo "Chef DK already installed"
-fi
-
+#!/usr/bin/env bash
 ##########################################
 # Change shell to zsh, if not already done
 #
-if [ "$(echo "$SHELL" | grep -c "zsh")" -eq "0" ]; then
-    echo "Setting shell to zsh"
-    chsh -s "$(which zsh)"
-else
-    echo "zsh is already the default shell"
-fi
+change_to_zsh() {
+    if [ "$(echo "$SHELL" | grep -c "zsh")" -eq "0" ]; then
+        echo "Setting shell to zsh"
+        chsh -s "$(which zsh)"
+    else
+        echo "zsh is already the default shell"
+    fi
+}
 
 #############################################
 # Create ssh dir with appropriate permissions
 #
-mkdir -p "$HOME"/.ssh
-chmod 0700 "$HOME"/.ssh
+create_ssh() {
+    mkdir -p "$HOME"/.ssh
+    chmod 0700 "$HOME"/.ssh
+}
+
+nix_install() {
+    if [ -d /nix ]; then
+        nix-env -j 4 -i minEnv bigEnv
+    else
+        echo "NixPkg not installed on this machine. Run this to install:"
+        echo "curl https://nixos.org/nix/install | sh"
+    fi
+}
+
+change_to_zsh
+create_ssh
+nix_install
