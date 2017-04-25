61b2f87b9c6ba5e83525749de08e4281d4c1fbbe
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-04-25 16:17:03 -0400

    Switch to profile

--- bin/install-multiuser-nix.sh
+++ bin/install-multiuser-nix.sh
@@ -68,15 +68,15 @@ nix-setup-user() {
     echo "Creating profile $PROFILE_DIR..."
     echo "Profile symlink: $SYMLINK_PATH"
 
-    rm "$SYMLINK_PATH"
+    rm -rf "$SYMLINK_PATH"
     mkdir -p "$PROFILE_DIR"
     chown "$TARGET_USER:$TARGET_USER" "$PROFILE_DIR"
 
     ln -s "$PROFILE_DIR/profile" "$SYMLINK_PATH"
     chown -h "$TARGET_USER:$TARGET_USER" "$SYMLINK_PATH"
 
-    echo "export NIX_REMOTE=daemon" >> "/home/$TARGET_USER/.bashrc"
-    echo ". /usr/local/etc/profile.d/nix.sh" >> "/home/$TARGET_USER/.bashrc"
+    echo "export NIX_REMOTE=daemon" >> "/home/$TARGET_USER/.profile"
+    echo ". /usr/local/etc/profile.d/nix.sh >> "/home/$TARGET_USER/.profile"
 
     su -lc "cd; . /usr/local/etc/profile.d/nix.sh; NIX_REMOTE=daemon nix-channel --update" "$TARGET_USER"
 }
