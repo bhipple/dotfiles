8a8692ddb3924babedc56146efefe4825d8225b9
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-08-28 18:34:33 -0400

    Update e and fe, add fo

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -105,12 +105,28 @@ fh() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
 
-# fe - Open a file with `e`
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
 fe() {
     local files
     IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
-    [[ -n "$files" ]] && e "${files[@]}"
+    [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
+
+# Modified version where you can press
+#   - CTRL-O to open with `open` command,
+#   - CTRL-E or Enter key to open with the $EDITOR
+fo() {
+    local out file key
+    IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
+    key=$(head -1 <<< "$out")
+    file=$(head -2 <<< "$out" | tail -1)
+    if [ -n "$file" ]; then
+        [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+    fi
+}
+
 
 # fbr - checkout git branch
 fbr() {
