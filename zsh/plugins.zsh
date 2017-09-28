b27cf4883c1781f2e32dbd50068608f03a2613b0
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-09-27 21:59:02 -0400

    Switch back to Auto-Fu
    Leaving zsh-autosuggestions installed to let me know when I can jjk easily, but
    changing the completer stuff to auto-fu.
    
    This reverts commit 440318c00c29d11a181c733226990d98310c90a3.
    This reverts commit e8d9a381b04edeafea49c614d4dec4b2859b9879.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,6 +1,9 @@
 # load zgen
-bindkey -v
+# auto-fu expects us to be in emacs mode before sourcing (wtf)
+bindkey -e
+bindkey $'\e' vi-cmd-mode
 source "${HOME}/.dotfiles/zgen/zgen.zsh"
+bindkey -v
 
 # Check only when there's no init script
 # To regenerate zgen config from scratch, rm ~/.zgen/init.zsh
@@ -16,6 +19,7 @@ if ! zgen saved; then
 
     # Github plugins
     zgen load djui/alias-tips
+    zgen load hchbaw/auto-fu.zsh . pu
     zgen load rupa/z
     zgen load zsh-users/zsh-autosuggestions
     zgen load zsh-users/zsh-completions
