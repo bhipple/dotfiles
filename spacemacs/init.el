61ceaa17115b988f8dd53593d4a49f0c71109f83
Benjamin Hipple <benjamin.hipple@gmail.com>
2017-10-24 08:38:29 -0400

    WIP: Defer loading of infrequently used spacemacs packages

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -100,7 +100,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(evil-ediff groovy-mode org-jira rpm-spec-mode)
+   dotspacemacs-additional-packages '(evil-ediff)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
--- spacemacs/layers/brh-general/packages.el
+++ spacemacs/layers/brh-general/packages.el
@@ -1,3 +1,20 @@
 ;;; packages.el --- brh-general layer packages file for Spacemacs.
 ;;
-(defconst brh-general-packages '())
+(defconst brh-general-packages
+  '(groovy-mode
+    jenkins
+    org-jira
+    rpm-spec-mode
+    ))
+
+(defun brh-general/init-groovy-mode ()
+  (use-package groovy-mode))
+
+(defun brh-general/init-jenkins ()
+  (use-package jenkins))
+
+(defun brh-general/init-org-jira ()
+  (use-package org-jira))
+
+(defun brh-general/init-rpm-spec-mode ()
+  (use-package rpm-spec-mode))
