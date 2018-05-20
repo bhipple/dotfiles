a6192430e8d6da4dfad2d7affdaa6ddc7140fb34
Benjamin Hipple <bhipple@protonmail.com>
2018-05-20 12:18:36 -0400

    nix-update

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -125,7 +125,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(evil-ediff jenkins org-jira rpm-spec-mode package-lint)
+   dotspacemacs-additional-packages '(evil-ediff jenkins org-jira rpm-spec-mode package-lint nix-update)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
