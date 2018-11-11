23fce42c39999ee17eab15762aa43a1cf10f9144
Benjamin Hipple <bhipple@protonmail.com>
2018-11-11 15:26:08 -0500

    Fix evil-escape via overlay patch

--- config/nixpkgs/overlays/envs.nix
+++ config/nixpkgs/overlays/envs.nix
@@ -1,6 +1,18 @@
 self: super:
 {
   spacemacs = super.emacsWithPackages (ep: (with ep.melpaPackages; [
+    # there's a bug in the current source of evil-escape that causes it to
+    # fail to build. We'll patch it out for now and hope it gets fixed in a
+    # future version.
+    (ep.evil-escape.overrideAttrs (old: {
+      patches = (old.patches or []) ++ [
+        (super.fetchpatch {
+          url = https://github.com/BrianHicks/evil-escape/commit/b548e8450570a0c8dea47b47221b728c047a9baf.patch;
+          sha256 = "1a2qrf4bpj7wm84qa3haqdg3pd9d8nh5vrj8v1sc0j1a9jifsbf6";
+        })
+      ];
+    }))
+
     # company-rtags
     # doom-modeline # TODO: Has a dependency that fails (shrink-path.el)
     # ep.font-lock-plus # TODO: Fails with mismatched package name font-lock+
@@ -106,7 +118,6 @@ self: super:
     evil-args
     evil-cleverparens
     evil-ediff
-    evil-escape
     evil-exchange
     evil-goggles
     evil-iedit-state
