98c9743bf5b0abdd4733633579dd9cb37c02ea39
Benjamin Hipple <bhipple@protonmail.com>
2018-12-29 22:29:53 -0500

    Leave MKL commented by default

--- config/nixpkgs/overlays/mkl.nix
+++ config/nixpkgs/overlays/mkl.nix
@@ -1,17 +1,19 @@
 self: super: {
-  python = super.python.override {
-    packageOverrides = python-self: python-super: {
-      numpy = python-super.numpy.override {
-        blas = super.pkgs.mkl;
-      };
-    };
-  };
 
-  python3 = super.python3.override {
-    packageOverrides = python-self: python-super: {
-      numpy = python-super.numpy.override {
-        blas = super.pkgs.mkl;
-      };
-    };
-  };
+  # Leave this commented out by default to get better Hydra cache efficiency
+  # python = super.python.override {
+  #   packageOverrides = python-self: python-super: {
+  #     numpy = python-super.numpy.override {
+  #       blas = super.pkgs.mkl;
+  #     };
+  #   };
+  # };
+
+  # python3 = super.python3.override {
+  #   packageOverrides = python-self: python-super: {
+  #     numpy = python-super.numpy.override {
+  #       blas = super.pkgs.mkl;
+  #     };
+  #   };
+  # };
 }
