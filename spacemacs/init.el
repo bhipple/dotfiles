0ca9cfb5d1dff214bb42224ccfc3770dadabcf6d
Benjamin Hipple <bhipple@protonmail.com>
2019-01-19 15:18:12 -0500

    Dracula as last preference

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -258,8 +258,8 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(solarized-dark
-                         dracula
-                         spacemacs-dark)
+                         spacemacs-dark
+                         dracula)
 
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
    ;; `all-the-icons', `custom', `doom', `vim-powerline' and `vanilla'. The
