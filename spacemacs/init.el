d1fe549fc9cbd78c2edb8ac2c8c909a585986a7d
Benjamin Hipple <bhipple@protonmail.com>
2019-02-02 10:41:22 -0500

    Float the size instead of pinning it
    Per comment from github, this should cause emacs to correctly scale based on
    the DPI.

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -277,7 +277,6 @@ It should only modify the values of Spacemacs settings."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
                                :weight normal
                                :width normal
                                :powerline-scale 1.0)
