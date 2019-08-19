7f0d670637a1516da3ae6a563dc983298f265cf6
Benjamin Hipple <bhipple@protonmail.com>
2019-08-19 19:59:12 -0400

    Faster zsh paste with autosuggest enabled

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -73,3 +73,19 @@ if [ -d ~/.fzf/shell ]; then
     source ~/.fzf/shell/completion.zsh
     source ~/.fzf/shell/key-bindings.zsh
 fi
+
+#  ============================================================================
+#                           Autosuggest Config
+#  ============================================================================
+# This speeds up pasting w/ autosuggest
+# https://github.com/zsh-users/zsh-autosuggestions/issues/238
+pasteinit() {
+    OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+    zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+
+pastefinish() {
+    zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
