9e49b9fbcfeb6e0dee26b66c13824fc5164d9efb
Benjamin Hipple <bhipple@protonmail.com>
2019-10-20 16:46:04 -0400

    Update xinitrc to disable TrackPad

--- config/nixpkgs/overlays/envs.nix
+++ config/nixpkgs/overlays/envs.nix
@@ -6,8 +6,8 @@ self: super:
     # There's a bug in the current source of evil-escape that causes it to
     # fail to build. We'll patch it out for now and hope it gets fixed in a
     # future version.
-    (ep.evil-escape.overrideAttrs (old: {
-      patches = (old.patches or []) ++ [
+    (ep.evil-escape.overrideAttrs (o: {
+      patches = (o.patches or []) ++ [
         (super.fetchpatch {
           url = https://github.com/BrianHicks/evil-escape/commit/b548e8450570a0c8dea47b47221b728c047a9baf.patch;
           sha256 = "1a2qrf4bpj7wm84qa3haqdg3pd9d8nh5vrj8v1sc0j1a9jifsbf6";
