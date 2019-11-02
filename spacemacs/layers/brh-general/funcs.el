9191108cae042370177092152045af8b668e00a5
Benjamin Hipple <bhipple@protonmail.com>
2019-11-02 13:56:42 -0400

    Use async-shell-command for sending cmds to tmux

--- spacemacs/layers/brh-general/funcs.el
+++ spacemacs/layers/brh-general/funcs.el
@@ -98,7 +98,7 @@
 (defun brh/_tmux-cmd (cmd)
   "Send a command to the active tmux terminal session. Also saves the buffer"
   (save-buffer 0)
-  (shell-command (concat "tmux send-keys '" cmd "' Enter")))
+  (async-shell-command (concat "tmux send-keys '" cmd "' Enter")))
 
 (defun brh/helm-run-shell ()
   "Interactively select a cmd to run in the shell with tmux"
