b4a09f7cd9157765d21e4fe74746c9ff74403c66
Benjamin Hipple <bhipple@protonmail.com>
2019-11-03 09:22:02 -0500

    Looks nicer this way

--- config/nixpkgs/overlays/emacs.nix
+++ config/nixpkgs/overlays/emacs.nix
@@ -1,8 +1,9 @@
 let
   rev = "c36610f3c986f8699a85efb9978ed602da40d4e2";
   sha256 = "1wnhy6z8zv2b00wrjrs5205ry0g8lamjb905zf14cxsgs0fz8dd6";
-in
-  import (builtins.fetchTarball {
+  emacs-overlay = import (builtins.fetchTarball {
     url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
     inherit sha256;
-  })
+  });
+in
+  emacs-overlay
