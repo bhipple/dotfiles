bd1da9dea774e26c718ec7069cb79e623d5c08ca
Benjamin Hipple <bhipple@protonmail.com>
2020-01-13 00:15:32 -0500

    Bugfix on emacs-overlay script

--- config/nixpkgs/overlays/emacs-overlay.nix
+++ config/nixpkgs/overlays/emacs-overlay.nix
@@ -1,6 +1,6 @@
 let
-  rev = "1e778bc5ca91b78d33673dfaddf131a7434ab322"
-  sha256 = "0svwagz0yxg2lvhx526n9hj9ggxmnkkx4y59hl55c0146sw0xxrk"
+  rev = "1e778bc5ca91b78d33673dfaddf131a7434ab322";
+  sha256 = "0svwagz0yxg2lvhx526n9hj9ggxmnkkx4y59hl55c0146sw0xxrk";
   emacs-overlay = import (builtins.fetchTarball {
     url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
     inherit sha256;
