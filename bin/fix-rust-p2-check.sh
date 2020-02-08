67f48eed10a4b9e6b1c69c591c74568446a65fa4
Benjamin Hipple <bhipple@protonmail.com>
2020-02-08 13:47:20 -0500

    Working lint for all of the new pkgs

---
+++ bin/fix-rust-p2-check.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+cd ~/src/nixpkgs || exit 1
+
+ATTRS=(
+    # All of the new fetchCargo packages
+    broot fido2luks gitAndTools.git-workspace hexdino mdsh nix-du tre-command wagyu
+    # Plus a spot-check of older ones
+    cargo rustc rx ripgrep exa
+)
+
+# Re-run the fix-rust-p2 script and verify no hash changes
+rm -rf old new
+rm -f result* || true
+git checkout -f feature/fetchcargo-tar-gz || true
+mkdir -p old new
+nix build -L -f . ${ATTRS[@]}
+
+mv result* old/
+fix-rust-p2.sh
+nix build -L -f . ${ATTRS[@]}
+mv result* new/
+
+set +x
+tree old new
+echo "********************************************************************************"
+echo "Diff"
+if ! diff -r old new; then
+    echo "There's a diff!"
+else
+    echo "No hash diff generated. Success!"
+fi
--- bin/fix-rust-p2.sh
+++ bin/fix-rust-p2.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env zsh
 set -euo pipefail
+# Once staging hits master, run this and send the treewide PR to master. This
+# should not cause any hash changes.
 cd ~/src/nixpkgs || exit 1
 git checkout -f feature/fetchcargo-tar-gz
 
-sed -i 's|^, legacyCargoFetcher.*|, legacyCargoFetcher ? false|' pkgs/build-support/rust/default.nix
+fix() {
+    sed -i 's|^, legacyCargoFetcher.*|, legacyCargoFetcher ? false|' pkgs/build-support/rust/default.nix
 
-for f in $(rg -l 'cargoSha256 = "' **/*.nix); do
-    if rg -q 'legacyCargoFetcher = false;' $f; then
-        continue
-    fi
+    for f in $(rg -l 'cargoSha256 = "' **/*.nix); do
+        # Leave the opt-outs for the first big wave, then delete them in another follow up (there are only 8).
+        if rg -q 'legacyCargoFetcher = false;' $f; then
+            continue
+        fi
 
-    sed -i '/cargoSha256 = "/i   # Delete this on next update\n  legacyCargoFetcher = true;\n' $f
-done
+        sed -i '/cargoSha256 = "/i   # Delete this on next update\n  legacyCargoFetcher = true;\n' $f
+    done
+}
+
+fix
