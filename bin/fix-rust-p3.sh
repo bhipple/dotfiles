6b90c3f41c2d2d2c2ce42c726f3bb24f71535da3
Benjamin Hipple <bhipple@protonmail.com>
2020-02-15 15:55:33 -0500

    For the broken ones

--- bin/fix-rust-p3.sh
+++ bin/fix-rust-p3.sh
@@ -10,13 +10,25 @@ git reset --hard origin/master
 fix-rust.sh $ATTR
 git diff
 
-MSG="""\
+# Covers almost all of them, fortunately
+HAPPY="""\
 $ATTR: upgrade cargo fetcher implementation and cargoSha256
 
 Infra upgrade as part of #79975; no functional change expected.
 """
 
-git commit -am"$MSG"
+# Missed a few, alas :(
+# Fortunately, they're broken on the "legacy" implementation only, so we can
+# just upgrade.
+SAD="""\
+$ATTR: fix build by migrating off legacy fetchCargo implementation
+
+Currently broken; see #79975 for details. Would also be fixed by #80153
+eventually, but since we want to upgrade either way we might as well do so now.
+"""
+
+git commit -am"$HAPPY"
+#git commit -am"$SAD"
 
 git push $USER
 hub pull-request
