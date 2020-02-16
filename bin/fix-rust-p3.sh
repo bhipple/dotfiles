1dd3c9c45b31ab8c3fdff1d147b9c6bd83230f1e
Benjamin Hipple <bhipple@protonmail.com>
2020-02-16 17:14:47 -0500

    Be friendly to community resources and queues

--- bin/fix-rust-p3.sh
+++ bin/fix-rust-p3.sh
@@ -21,11 +21,15 @@ git commit -am"$MSG"
 # Auto-PR via hub
 git push -f $USER
 rm -f hub-log
+
+# TODO: Before running again, rate limit this with something like:
+# while [ $(curl https://events.nix.ci/stats.php | jq -r .evaluator.messages.waiting) -gt 1 ]; do sleep 30; done
 hub pull-request --no-edit | tee hub-log
 
 PR=$(grep nixpkgs hub-log | sed 's|.*pull/||')
 TOKEN=$(grep oauth_token ~/.config/hub | awk '{print $NF}')
 
 # Tell ofBorg to build it!
-curl -s -H "Authorization: token $TOKEN" -XPOST \
-    -d "{\"body\": \"@grahamcofborg build $ATTR\"}" https://api.github.com/repos/NixOS/nixpkgs/issues/$PR/comments
+# Per comment from Graham C, this is unnecessary
+# curl -s -H "Authorization: token $TOKEN" -XPOST \
+#     -d "{\"body\": \"@grahamcofborg build $ATTR\"}" https://api.github.com/repos/NixOS/nixpkgs/issues/$PR/comments
