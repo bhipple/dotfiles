dbaecab51c01f6c37b8f51473c339ca96cd7b5f7
Benjamin Hipple <bhipple@protonmail.com>
2020-03-02 12:20:10 -0500

    Longer timeout for gcmh in spacemacs

--- spacemacs/layers/brh-general/config.el
+++ spacemacs/layers/brh-general/config.el
@@ -111,6 +111,9 @@
 ;; Set the max GC run at 2GB of RAM
 (setq gcmh-high-cons-threshold #x80000000)
 
+;; Run after 60 seconds of idle time, not 15
+(setq gcmh-idle-delay 60)
+
 ;; Per suggestion on lsp-mode, increase read-process output
 ;; https://github.com/emacs-lsp/lsp-mode/#performance
 (setq read-process-output-max (* 4 1024 1024)) ;; 4mb
