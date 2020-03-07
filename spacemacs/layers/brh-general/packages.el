073ec9475517c7a968fbd8ca5f00cfd258ddb18b
Benjamin Hipple <bhipple@protonmail.com>
2020-03-07 18:46:13 -0500

    xclip and tmux-pane plugins too slow

--- spacemacs/layers/brh-general/packages.el
+++ spacemacs/layers/brh-general/packages.el
@@ -1,6 +1,6 @@
 ;;; packages.el --- brh-general layer packages file for Spacemacs.
 ;;
-(defconst brh-general-packages '(direnv gcmh nix-update s solarized-theme tmux-pane))
+(defconst brh-general-packages '(direnv gcmh nix-update s solarized-theme))
 
 (defun brh-general/init-direnv ()
   (use-package direnv
@@ -19,11 +19,3 @@
 
 (defun brh-general/init-solarized-theme ()
   (use-package solarized-theme))
-
-(defun brh-general/init-tmux-pane ()
-  (use-package tmux-pane))
-
-;; (defun brh-general/init-xclip ()
-;;   (use-package xclip
-;;    :init
-;;    (when (not (display-graphic-p)) (xclip-mode))))
