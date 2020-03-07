f539670f714aac90f0073f0606a36f9949e96867
Benjamin Hipple <bhipple@protonmail.com>
2020-03-07 10:36:20 -0500

    Simplify tag creation
    I'm no longer using GNU Global or Haskell fast-tags, so these can just be removed

--- git_template/hooks/update-tags.sh
+++ git_template/hooks/update-tags.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# Creates ctags, gtags, and haskell fast-tags
+#!/usr/bin/env sh
+# Creates ctags after commits
 set -e
 PATH="/usr/local/bin:$PATH"
 dir="$(git rev-parse --git-dir)"
@@ -9,14 +9,3 @@ trap 'rm -f "$dir/$$.tags"' EXIT
 git ls-files | \
           ctags - -f"$dir/$$.tags"
           mv "$dir/$$.tags" "$dir/tags"
-
-# GNU Global Tags
-mkdir "$dir/$$.gtags"
-git ls-files | \
-    gtags --file=- --skip-unreadable "$dir/$$.gtags"
-mv "$dir"/$$.gtags/* "$dir/.."
-
-# Haskell Fast-Tags
-if hash fast-tags > /dev/null 2>&1; then
-    git ls-files '*.hs' | xargs fast-tags -o .hs-tags
-fi
