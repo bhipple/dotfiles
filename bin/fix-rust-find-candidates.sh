bcd7765afc78b37c94d5db47514c6a0edba193fa
Benjamin Hipple <bhipple@protonmail.com>
2020-03-11 17:51:46 -0400

    Rust touchup

--- bin/fix-rust-find-candidates.sh
+++ bin/fix-rust-find-candidates.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 for f in $(rg -l "Delete this on next update"); do
-    if rg -q "broken = true;" $f; then continue; fi
-    if rg -q "sourceRoot =" $f; then continue; fi
-    if rg -q "cargoPatches =" $f; then continue; fi
-    if rg -q "Cargo.lock" $f; then continue; fi
+    # if rg -q "broken = true;" $f; then continue; fi
+    # if rg -q "sourceRoot =" $f; then continue; fi
+    if rg -q "cargoPatches =" $f; then echo $f; fi
+    if rg -q "Cargo.lock" $f; then echo $f; fi
     echo $f
 done
