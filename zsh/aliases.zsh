0a368c9dd04f77ad4c93334a951fba5d9b4eabfd
Benjamin Hipple <bhipple@protonmail.com>
2020-07-09 16:01:03 -0400

    X alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -50,6 +50,7 @@ if [[ -n "$ZSH_NAME" ]]; then
     alias -g S='| sort'
     alias -g SUM='| paste -sd+ - | bc'
     alias -g W='| wc -l'
+    alias -g X='| xargs'
 fi
 
 # Remove this alias from oh-my-zsh
