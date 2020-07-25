2d6d15fa5a9dd834ec83c36aea5f4ccd598451f8
Benjamin Hipple <bhipple@protonmail.com>
2020-07-25 11:14:38 -0400

    Fix emc

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -105,7 +105,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask,               xK_p     ), spawn "dmenu_run")
     , ((modMask .|. shiftMask, xK_b     ), spawn "chromium")
     , ((modMask .|. shiftMask, xK_c     ), kill)
-    , ((modMask .|. shiftMask, xK_e     ), spawn "emc")
+    , ((modMask .|. shiftMask, xK_e     ), spawn "emacsclient -c")
     , ((modMask .|. shiftMask, xK_f     ), spawn "firefox")
     , ((modMask .|. shiftMask, xK_x     ), spawn "~/.xinitrc")
     , ((controlMask .|. shiftMask, xK_l), spawn "sudo /run/current-system/sw/bin/slock")
