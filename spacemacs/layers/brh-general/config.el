3ae4f3c701ed00aaf6dd9c38bd44b418d91d28f1
Benjamin Hipple <bhipple@protonmail.com>
2020-08-11 16:15:59 -0400

    Tramp fix for git-gutter

--- spacemacs/layers/brh-general/config.el
+++ spacemacs/layers/brh-general/config.el
@@ -176,3 +176,24 @@
 
 (add-hook 'sh-mode-hook
           (lambda () (setq-local dash-docs-docsets '("Bash"))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Bugs / Workarounds that can hopefully be deleted someday
+
+; Workaround for:
+; https://github.com/nonsequitur/git-gutter-plus/issues/42
+; https://github.com/syl20bnr/spacemacs/issues/12860
+(with-eval-after-load 'git-gutter+
+  (defun git-gutter+-remote-default-directory (dir file)
+    (let* ((vec (tramp-dissect-file-name file))
+           (method (tramp-file-name-method vec))
+           (user (tramp-file-name-user vec))
+           (domain (tramp-file-name-domain vec))
+           (host (tramp-file-name-host vec))
+           (port (tramp-file-name-port vec)))
+      (tramp-make-tramp-file-name method user domain host port dir)))
+
+  (defun git-gutter+-remote-file-path (dir file)
+    (let ((file (tramp-file-name-localname (tramp-dissect-file-name file))))
+      (replace-regexp-in-string (concat "\\`" dir) "" file)))
