66d92436e60b3dd055a763f384b69babe98a7d78
Benjamin Hipple <bhipple@protonmail.com>
2021-10-31 11:06:44 -0400

    UTF-8 everywhere

--- spacemacs/layers/brh-general/config.el
+++ spacemacs/layers/brh-general/config.el
@@ -83,6 +83,17 @@
 (add-hook 'mouse-leave-buffer-hook #'kill-minibuffer)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; UTF-8 everywhere, as detailed in
+;; https://www.masteringemacs.org/article/working-coding-systems-unicode-emacs
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; LSP Settings
 
 (with-eval-after-load 'lsp-mode
