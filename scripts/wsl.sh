2ecc4b9b22203a0f275f2f3bca4ed468d73aa126
Benjamin Hipple <bhipple@protonmail.com>
2022-12-25 06:20:50 -0500

    Default cursor shape

--- scripts/wsl.sh
+++ scripts/wsl.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-set -euxo pipefail
+set -euo pipefail
 
 if ! [[ -d /mnt/wsl ]]; then
     exit
@@ -8,9 +8,11 @@ fi
 echo "Linking WSL scripts"
 
 for d in /mnt/c/Users/*/.glaze-wm; do
+    echo "Copiyng Glaze config to $d"
     cp ~/dotfiles/wsl/glaze-wm/config.yaml $d/
 done
 
 for f in /mnt/c/Users/*/AppData/Local/Packages/Microsoft.WindowsTerminal_*/LocalState/settings.json; do
+    echo "Copiyng Windows Terminal settings to $f"
     cp ~/dotfiles/wsl/windows-terminal/settings.json $f
 done
