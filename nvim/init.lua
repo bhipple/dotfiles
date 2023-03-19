9eab62c027ab89333aea8cd6f06759d37fcc3ca0
Benjamin Hipple <bhipple@protonmail.com>
2023-03-19 17:40:54 -0400

    git files by default

--- nvim/init.lua
+++ nvim/init.lua
@@ -281,7 +281,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<C-p>',      require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<C-p>',      require('telescope.builtin').git_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>pf', require('telescope.builtin').git_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 
