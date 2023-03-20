33653fc565aa4c4e3b76170e8a224e31cb1bbc66
Benjamin Hipple <bhipple@protonmail.com>
2023-03-20 09:15:00 -0400

    Remove autocomplete; it's too slow

--- spacemacs/layers/brh-general/config.el
+++ spacemacs/layers/brh-general/config.el
@@ -58,8 +58,6 @@
 ;; auto-indent. Auto messes up yaml, python, salt files, etc.
 (setq yas-indent-line 'fixed)
 
-(add-hook 'after-init-hook 'company-tng-mode)
-
 ;; Tell Spacemacs to put org clocks on my modeline by default
 (setq spaceline-org-clock-p t)
 
