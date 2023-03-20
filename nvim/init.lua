fdf37aaa2c439fd5fa2a6c87aef57e5536e10149
Benjamin Hipple <bhipple@protonmail.com>
2023-03-20 11:21:36 -0400

    nvim cmp buffer

--- nvim/init.lua
+++ nvim/init.lua
@@ -98,6 +98,7 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
+  'hrsh7th/cmp-buffer',
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
