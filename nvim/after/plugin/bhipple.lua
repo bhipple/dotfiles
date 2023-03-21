760bff30ccd8f39656fdbf6b3bd454bbc202c0ea
Benjamin Hipple <bhipple@protonmail.com>
2023-03-21 18:23:37 -0400

    Cleanup: use a nixpkgs branch instead of submodule, remove some kickstart files

---
+++ nvim/after/plugin/bhipple.lua
@@ -0,0 +1,11 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
