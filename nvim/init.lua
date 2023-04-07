1a749e812d34cfd34b719315afca08b74ab82f4c
Benjamin Hipple <bhipple@protonmail.com>
2023-04-07 10:05:34 -0400

    lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -339,11 +339,8 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+  clangd = {},
+  pylsp = {},
 }
 
 -- Setup neovim lua configuration
