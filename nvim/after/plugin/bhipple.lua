958fd66817df41449e0e9570d347d44e6fdb5f50
Benjamin Hipple <bhipple@protonmail.com>
2023-05-27 10:12:22 -0400

    Terminal maps

--- nvim/after/plugin/bhipple.lua
+++ nvim/after/plugin/bhipple.lua
@@ -66,6 +66,10 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
+-- Terminal keymaps
+vim.keymap.set("n", "<leader>tt", ":!", { desc = '[Terminal] command run synchronously' })
+vim.keymap.set("n", "<leader>to", ":terminal\n", { desc = '[T]erminal [O]pen' })
+
 -- Trouble keymaps
 vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", {silent = true, noremap = true})
 vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", {silent = true, noremap = true})
