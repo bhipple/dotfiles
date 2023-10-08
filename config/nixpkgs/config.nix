7afafa46051eb8818e35b641d083e56fb8bbb3a0
Benjamin Hipple <bhipple@protonmail.com>
2023-10-08 13:33:49 -0400

    secure

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -9,7 +9,10 @@
   };
 
   permittedInsecurePackages = [
-    "python3.10-cryptography-3.4.8"
     "openssl-1.1.1v"
+    "openssl-1.1.1w"
+    "python3.10-cryptography-3.4.8"
+    "python3.10-cryptography-40.0.1"
+    "python3.10-requests-2.28.2"
   ];
 }
