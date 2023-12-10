4bfc4907a023c8255a351f524a29e0d37239aaa7
Benjamin Hipple <bhipple@protonmail.com>
2023-12-10 09:16:18 -0500

    trim insecure

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -9,10 +9,6 @@
   };
 
   permittedInsecurePackages = [
-    "openssl-1.1.1v"
     "openssl-1.1.1w"
-    "python3.10-cryptography-3.4.8"
-    "python3.10-cryptography-40.0.1"
-    "python3.10-requests-2.28.2"
   ];
 }
