e4c8e06361daa7be0f4f84cfc78185801bde4ce3
Benjamin Hipple <bhipple@protonmail.com>
2024-06-04 10:09:24 -0400

    zsh autofetch

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -8,13 +8,13 @@ if ! zgenom saved; then
     echo "Creating a zgenom save"
 
     export DISABLE_AUTO_UPDATE="true"  # Don't ask for oh-my-zsh prompts
-    zgenom oh-my-zsh
-
-    zgenom load romkatv/powerlevel10k powerlevel10k
+    zgenom ohmyzsh
+    zgenom ohmyzsh plugins/git-auto-fetch
 
     # Github plugins
-    zgenom load djui/alias-tips
+    zgenom load romkatv/powerlevel10k powerlevel10k
     zgenom load romkatv/zsh-prompt-benchmark
+    zgenom load djui/alias-tips
     zgenom load zsh-users/zsh-syntax-highlighting
 
     # save all to init script
