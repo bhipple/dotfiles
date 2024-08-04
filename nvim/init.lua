34ef0622cbf87a41b1f558892130cbe5a6405c37
Benjamin Hipple <bhipple@protonmail.com>
2024-08-04 11:15:11 -0400

    nvim lua lsp working

--- nvim/init.lua
+++ nvim/init.lua
@@ -341,22 +341,11 @@ require('lspconfig').lua_ls.setup {
     end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT'
-      },
+      runtime = { version = 'LuaJIT' },
       -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME
-          -- Depending on the usage, you might want to add additional paths here.
-          -- "${3rd}/luv/library"
-          -- "${3rd}/busted/library",
-        }
-        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-        -- library = vim.api.nvim_get_runtime_file("", true)
+        library = vim.api.nvim_get_runtime_file("", true),
       }
     })
   end,
