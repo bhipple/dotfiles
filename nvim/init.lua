77c91d8340075dc9b67343bae398c4c8ec53731c
Benjamin Hipple <bhipple@protonmail.com>
2024-08-04 11:07:16 -0400

    lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,10 +47,6 @@ require('lazy').setup({
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', tag = "legacy", opts = {} },
@@ -319,16 +315,6 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- Enable the following language servers; full list here:
--- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
-local servers = {
-  bashls = {},
-  clangd = {},
-  pyright = {},
-  terraformls = {},
-  yamlls = {},
-}
-
 -- Setup neovim lua configuration
 require('neodev').setup()
 
@@ -347,24 +333,36 @@ require('lspconfig')['yamlls'].setup {
   }
 }
 
--- Setup mason so it can manage external tooling
-require('mason').setup()
+require('lspconfig').lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      return
+    end
 
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
   end,
+  settings = {
+    Lua = {}
+  }
 }
 
 --------------------------------------------------------------------------------
