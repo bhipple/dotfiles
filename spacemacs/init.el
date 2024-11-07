063d78a7874f14e33fcaddc4f8b8cc6bb2749f34
Benjamin Hipple <bhipple@protonmail.com>
2024-11-07 11:06:03 -0500

    simplify org

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -72,9 +72,6 @@ This function should only modify configuration layer settings."
      markdown
      nixos
      (org :variables
-          org-enable-bootstrap-support t
-          org-enable-github-support t
-          org-enable-jira-support t
           org-enable-org-journal-support t
           org-enable-roam-protocol t
           org-enable-roam-support t
