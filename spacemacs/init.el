42aa49a279609faae870768188cfbe14cf69ae1d
Benjamin Hipple <bhipple@protonmail.com>
2025-03-29 14:20:40 -0400

    emacs font

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -329,7 +329,7 @@ It should only modify the values of Spacemacs settings."
    ;; fixed-pitch faces. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("Source Code Pro"
+   dotspacemacs-default-font '("Inconsolata"
                                :size 10.0
                                :weight normal
                                :width normal)
